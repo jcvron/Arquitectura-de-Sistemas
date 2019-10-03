4731b270634dabd64570b96d2256dc7722b92ce5
tostin <tostin737@gmail.com>
2019-10-03 03:24:09 -0300

    Pequeños cambios de interfaz como base

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  get 'cursos/index'
+  get 'teachers/index'
+  get 'students/index'
+  resources :alumnos, controller: 'students'
+
+  resources :profesors, controller: 'teachers'
+
+  get 'welcome/index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
