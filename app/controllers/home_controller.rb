71ff444f63e44cb8ba6d941d3a9ecbaf83157dd2
tostin <tostin737@gmail.com>
2019-10-06 21:09:17 -0300

    Cursos para alumnos perfil

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
     def perfil
         @homes = Profesor.where user_id: current_user.id
         @casita = Alumno.where user_id: current_user.id
+        @cursitos = Curso.where alumno_id: current_user.id
     end
 end
